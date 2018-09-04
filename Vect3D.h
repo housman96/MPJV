2628c496029856a9a82813c06cb4502b1cadab9d
Griwald <griwald13@gmail.com>
2018-09-04 12:09:58 -0400

    Ajout des commentaires de la classe "Vect3D"

--- Vect3D.h
+++ Vect3D.h
@@ -5,6 +5,9 @@
 #include <cmath>
 
 
+/**
+ * Une classe de vecteurs 3D
+ */
 class Vect3D {
 private:
 	float x;
@@ -15,19 +18,21 @@ public:
 	Vect3D(float = 0, float = 0, float = 0); /* Constructeur par défaut */
 	Vect3D(const Vect3D &);                  /* Constructeur de recopie */
 
-	Vect3D add(const Vect3D &);
-	Vect3D scale(const float);
-	Vect3D mult(const Vect3D &);
-	float dot(const Vect3D &);
-	float mag();
-	float magSq();
-	Vect3D normalize();
-	static Vect3D proj(Vect3D &, Vect3D &);
-	static float dist(Vect3D &, Vect3D &);
-	static float angleBetween(Vect3D &, Vect3D &);
-	Vect3D cross(const Vect3D &);
+	Vect3D add(const Vect3D &);   /* Addition de deux vecteurs */
+	Vect3D scale(const float);    /* Multiplication par un scalaire */
+	Vect3D mult(const Vect3D &);  /* Multiplication des composantes */
+	float dot(const Vect3D &);    /* Produit scalaire */
+	Vect3D cross(const Vect3D &); /* Produit vectoriel */
 
-	void log();
+	float mag();        /* Norme du vecteur */
+	float magSq();      /* Norme du vecteur au carré */
+	Vect3D normalize(); /* Rend le vecteur unitaire */
+
+	static Vect3D proj(Vect3D &, Vect3D &);        /* Projection d'un vecteur sur un autre */
+	static float dist(Vect3D &, Vect3D &);         /* Distance entre deux points */
+	static float angleBetween(Vect3D &, Vect3D &); /* Angle entre deux vecteurs */
+
+	void log(); /* Affichage dans la console */
 };
 
 
