85f459a0416ad83395e9a1028b127367b69a4da0
querment <theo.querment96@gmail.com>
2018-09-10 18:59:53 +0200

    Mise en place de la boucle d'affichage

--- Affichage.cpp
+++ Affichage.cpp
@@ -7,18 +7,24 @@
 
 using namespace std;
 
+// ============================================================
+// METHODE GLUT
+// ============================================================
+
 void Affichage::Display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
 
     glPushMatrix();
-
+    cout << "Display" << Affichage::list.size() << endl;
     for (Particle part : Affichage::list)
     {
+        glPushMatrix();
         glTranslatef(part.getPosition().getX(), part.getPosition().getY(), part.getPosition().getZ());
+        glutSolidSphere(1.0, 50, 50);
+        glPopMatrix();
     }
-    glutSolidSphere(1.0, 50, 50);
 
     glPopMatrix();
 
@@ -42,6 +48,41 @@ void Affichage::redim(int width, int height)
     glLoadIdentity();
     gluPerspective(70.0, 1.0, 1.0, 10.0);
 }
+
+void Affichage::idle(void)
+{
+    double now = glutGet(GLUT_ELAPSED_TIME);
+    double timeElapsedMs = ((now - Affichage::lastLoopTime) * 1000) / (CLOCKS_PER_SEC);
+    Affichage::timeAccumulatedMs += timeElapsedMs;
+
+    while (Affichage::timeAccumulatedMs >= Affichage::T)
+    {
+        for (Particle p : Affichage::list)
+        {
+            Vect3D grav = new Vect3D(0, -G, 0);
+            p = new Particle(Vect3D(0, 0, 0), Vect3D(1, 1, 0), 10);
+            p.setDamping(D);
+
+            for (int i = 0; i < 10; i++)
+            {
+                p.applyForce(grav);
+                p.update(T);
+            }
+        }
+        glutPostRedisplay();
+    }
+
+    Affichage::lastLoopTime = now;
+}
+
+void Affichage::refresh(void)
+{
+    glutPostRedisplay();
+}
+
+// ============================================================
+// CONSTRUCTEURS
+// ============================================================
 
 Affichage::Affichage(int argc, char **argv, Particle part)
 {
@@ -67,6 +108,80 @@ Affichage::Affichage(int argc, char **argv, Particle part)
     /* Association des callback pour cette fenêtre */
     glutDisplayFunc(Affichage::Display);
     glutReshapeFunc(Affichage::redim);
+    glutIdleFunc(Affichage::idle);
+    glutMainLoop(); /* On entre dans la boucle d'événements */
+}
+
+Affichage::Affichage(int argc, char **argv)
+{
+
+    /* Initialisation de GLUT */
+
+    glutInit(&argc, argv);
+
+    /* Choix du type et d'affichage RGBA (mode couleur le plus fréquent), tampon de profondeur
+    et d'un double buffer */
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+
+    /* Taille et emplacement de la fenêtre */
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(200, 100);
+
+    /* Création de la fenêtre */
+    glutCreateWindow("GLUTfenetre");
+    glEnable(GL_DEPTH_TEST);
+
+    /* Association des callback pour cette fenêtre */
+    glutDisplayFunc(Affichage::Display);
+    glutReshapeFunc(Affichage::redim);
 
     glutMainLoop(); /* On entre dans la boucle d'événements */
+}
+
+Affichage::Affichage(int argc, char **argv, vector<Particle> list)
+{
+    Affichage::list = list;
+    /* Initialisation de GLUT */
+
+    glutInit(&argc, argv);
+
+    /* Choix du type et d'affichage RGBA (mode couleur le plus fréquent), tampon de profondeur
+    et d'un double buffer */
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+
+    /* Taille et emplacement de la fenêtre */
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(200, 100);
+
+    /* Création de la fenêtre */
+    glutCreateWindow("GLUTfenetre");
+    glEnable(GL_DEPTH_TEST);
+
+    /* Association des callback pour cette fenêtre */
+    glutDisplayFunc(Affichage::Display);
+    glutReshapeFunc(Affichage::redim);
+
+    glutMainLoop(); /* On entre dans la boucle d'événements */
+}
+
+Affichage::~Affichage()
+{
+    // Deallocate the memory that was previously reserved
+    //  for this list.
+    if (list.data())
+        delete[] & list;
+}
+
+// ============================================================
+// ASCESSEUR
+// ============================================================
+
+vector<Particle> Affichage::getList()
+{
+    return Affichage::list;
+}
+
+void Affichage::setList(vector<Particle> newList)
+{
+    Affichage::list = newList;
 }
--- Affichage.h
+++ Affichage.h
@@ -4,16 +4,31 @@
 #include <vector>
 #include "Particle.h"
 
+using namespace std;
+
 /**
  * La classe qui affiche tous les objets qu'on lui donne
  */
 class Affichage
 {
 private:
-  static std::vector<Particle> list;
+  static vector<Particle> list;
+  static float lastLoopTime;
+  static float timeAccumulatedMs;
+  static const int T;
+  static const float D;
+  static const int G;
 
 public:
   Affichage(int argc, char **argv, Particle part);
+  Affichage(int argc, char **argv);
+  ~Affichage();
+  Affichage(int argc, char **argv, vector<Particle> list);
+
+  static void idle(void);
+  void refresh(void);
+  void setList(vector<Particle> list);
+  vector<Particle> getList();
   static void Display();
   static void redim(int width, int height);
 };
