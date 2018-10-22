20aeeceb5996e520fc111b0de1dca39026a7b47f
Griwald <griwald13@gmail.com>
2018-10-21 20:15:45 -0400

    AnchoredSpringForce DONE SpringForceGenerator TODO BungeeSpringForceGenerator TODO

--- BungeeSpringForceGenerator.cpp
+++ BungeeSpringForceGenerator.cpp
@@ -1,37 +1,34 @@
 #include "BungeeSpringForceGenerator.h"
 
-BungeeSpringForceGenerator::BungeeSpringForceGenerator(const Particle &p, const float k_, const float l0_)
+BungeeSpringForceGenerator::BungeeSpringForceGenerator(const Particle& p, const float k_, const float l0_)
 {
     this->otherP = p;
     this->k = k_;
     this->l0 = l0_;
 }
 
-BungeeSpringForceGenerator::BungeeSpringForceGenerator(const BungeeSpringForceGenerator &other)
+BungeeSpringForceGenerator::BungeeSpringForceGenerator(const BungeeSpringForceGenerator& other)
 {
     otherP = other.otherP;
     k = other.k;
     l0 = other.l0;
 }
 
-BungeeSpringForceGenerator::BungeeSpringForceGenerator(const BungeeSpringForceGenerator *other)
+BungeeSpringForceGenerator::BungeeSpringForceGenerator(const BungeeSpringForceGenerator* other)
 {
     otherP = other->otherP;
     k = other->k;
     l0 = other->l0;
 }
 
-void BungeeSpringForceGenerator::updateForce(Particle *p, float duration)
+void BungeeSpringForceGenerator::updateForce(Particle* p, float duration)
 {
     Vect3D d = p->getPosition().sub(otherP.getPosition());
     Vect3D springForce;
 
-    if (d.mag() <= l0)
-    {
+    if (d.mag() <= l0) {
         springForce = new Vect3D(0, 0, 0);
-    }
-    else
-    {
+    } else {
         springForce = d.normalize();
         springForce = springForce.scale(-k * (d.mag() - l0));
     }
--- moteurJeu.cpp
+++ moteurJeu.cpp
@@ -1,9 +1,12 @@
 #include "Affichage.h"
+#include "AnchoredSpringForceGenerator.h"
+#include "BungeeSpringForceGenerator.h"
 #include "DragGenerator.h"
 #include "GravityGenerator.h"
 #include "Particle.h"
 #include "RegisterForce.h"
 #include "SpringForceGenerator.h"
+#include "StiffSpringForceGenerator.h"
 #include "Vect3D.h"
 #include "constants.h"
 #include <vector>
@@ -21,34 +24,40 @@ int main(int argc, char** argv)
 {
     /* DECLARATION DES PARTICULES */
     Particle p1 = new Particle(10., 1.);
-    Particle p2 = new Particle(2000., 1.);
-    p1.init(Vect3D(-2, 10, 0), Vect3D(-5, 0, 0), Vect3D(0, 0, 0));
+    Particle p2 = new Particle(20., 1.);
+    p1.init(Vect3D(-2, 5, 0), Vect3D(-5, 0, 0), Vect3D(0, 0, 0));
     p2.init(Vect3D(2, 10, 0), Vect3D(2, 0, 0), Vect3D(0, 0, 0));
 
     /* DECLARATION DES GENERATEURS DE FORCES */
     GravityGenerator gg = new GravityGenerator(Vect3D(0, -G, 0));
     DragGenerator dg = new DragGenerator(.9f, .7f);
-    SpringForceGenerator sfg = new SpringForceGenerator(p2, 15, 2);
-    //cout << "Main : " << &p2 << endl;
+    SpringForceGenerator sfg = new SpringForceGenerator(p2, 15.f, 2.f);
+    BungeeSpringForceGenerator bsfg = new BungeeSpringForceGenerator(p2, 15.f, 2.f);
+    AnchoredSpringForceGenerator asfg = new AnchoredSpringForceGenerator(Vect3D(0, 10, 0), 30.f, 0.5f);
 
     /* REMPLISSAGE DU REGISTRE DE FORCE */
-    // RegisterForce::ForceRecord fr_p1_gg;
-    // RegisterForce::ForceRecord fr_p1_dg;
-    RegisterForce::ForceRecord fr_p1_sfg;
+    //RegisterForce::ForceRecord fr_p1_gg;
+    //RegisterForce::ForceRecord fr_p1_dg;
+    //RegisterForce::ForceRecord fr_p1_sfg;
+    RegisterForce::ForceRecord fr_p1_bsfg;
+    //RegisterForce::ForceRecord fr_p1_asfg;
 
+    //fr_p1_gg.p = &p1;
+    //fr_p1_gg.pfg = &gg;
+    //fr_p1_dg.p = &p1;
+    //fr_p1_dg.pfg = &dg;
+    //fr_p1_sfg.p = &p1;
+    //fr_p1_sfg.pfg = &sfg;
+    fr_p1_bsfg.p = &p1;
+    fr_p1_bsfg.pfg = &bsfg;
+    //fr_p1_asfg.p = &p1;
+    //fr_p1_asfg.pfg = &asfg;
 
-    // fr_p1_gg.p = &p1;
-    // fr_p1_gg.pfg = &gg;
-    // fr_p1_dg.p = &p1;
-    // fr_p1_dg.pfg = &dg;
-    fr_p1_sfg.p = &p1;
-    fr_p1_sfg.pfg = &sfg;
-    // fr_p2_dg.p = &p2;
-    // fr_p2_dg.pfg = &dg;
-
-    // records.push_back(fr_p1_gg);
-    // records.push_back(fr_p1_dg);
-    records.push_back(fr_p1_sfg);
+    //records.push_back(fr_p1_gg);
+    //records.push_back(fr_p1_dg);
+    //records.push_back(fr_p1_sfg);
+    records.push_back(fr_p1_bsfg);
+    //records.push_back(fr_p1_asfg);
 
     /* CREATION DE L'AFFICHAGE */
     vector<Particle*> particules;
