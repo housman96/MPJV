1838607db1b716621cecc56e1f999cb8831efe4c
Théo <theo.querment96@gmail.com>
2018-10-24 10:37:14 -0400

    commentaires des particles cpp

--- ParticleContact.cpp
+++ ParticleContact.cpp
@@ -1,5 +1,9 @@
 #include "ParticleContact.h"
 
+// ============================================================
+// CONSTRUCTEURS
+// ============================================================
+
 ParticleContact::ParticleContact(Particle *p1, Particle *p2, float c_) {
 	particles[0] = p1;
 	particles[1] = p2;
@@ -9,7 +13,7 @@ ParticleContact::ParticleContact(Particle *p1, Particle *p2, float c_) {
 	Vect3D p2_pos = p2->getPosition();
 	n = p1_pos.sub(p2_pos).normalize();
 
-	d = (p1->getRadius() + p2->getRadius()) - (Vect3D::dist(p1_pos, p2_pos)); //d = 2*r - dist pa pb
+	d = (p1->getRadius() + p2->getRadius()) - (Vect3D::dist(p1_pos, p2_pos)); //d = r1+r2 - dist pa pb
 	Vs = calcVs();
 }
 
@@ -24,15 +28,18 @@ ParticleContact::ParticleContact(Particle *p1, float c_, Vect3D n, float d) {
 
 	this->n = n;
 
-	this->d = d; //d = 2*r - dist pa pb
+	this->d = d;
 	this->Vs = calcVs();
 }
 
-void ParticleContact::resolve() {
-	printf("hello");
-	resolveInterpenetration();
-	if (calcVs() < 0) {
+// ============================================================
+// RESOLUTION DES CONTACTS
+// ============================================================
 
+void ParticleContact::resolve() {
+
+	if (calcVs() < 0) {
+		resolveInterpenetration();
 		resolveVelocity();
 	}
 }
@@ -58,7 +65,7 @@ void ParticleContact::resolveVelocity() {
 	if (particles[1] != NULL) {
 		float m0 = particles[0]->getMass();
 		float m1 = particles[1]->getMass();
-		particles[0]->setVelocity(particles[0]->getVelocity().add(n.scale(m0*Vs*(-1 - c)/(m0+m1))));
+		particles[0]->setVelocity(particles[0]->getVelocity().add(n.scale(m0*Vs*(-1 - c) / (m0 + m1))));
 		particles[1]->setVelocity(particles[1]->getVelocity().add(n.scale(m1*Vs*(1 + c) / (m0 + m1))));
 	}
 	else {
